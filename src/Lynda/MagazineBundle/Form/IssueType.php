8d94be5ee171d8442e01c61a52cd2b8107403ef0
labnova <progettofeb@gmail.com>
2014-10-01 11:51:05 +0200

    version 1.0

---
+++ src/Lynda/MagazineBundle/Form/IssueType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Lynda\MagazineBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class IssueType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('number')
+            ->add('datePublication', 'date', array(
+                'years' =>range(date('Y'), date('Y', strtotime('-50 years'))),
+                'required' =>TRUE,
+            ))
+            ->add('file')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Lynda\MagazineBundle\Entity\Issue'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'lynda_magazinebundle_issue';
+    }
+}
